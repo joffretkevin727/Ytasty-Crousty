def4d92d8e9a314bbe52bfa9323c534630ee2831
Nikita Petrenko <nikita.petrenko@ynov.com>
2026-09-09 15:26:30 +0200

    remove import users router from main in auth branch

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
 
 from src.app.modules.auth.router import auth_router
-from src.app.modules.users.router import users_router
 
 app = FastAPI(title="Toasty-Crousty")
 
 app.include_router(auth_router)  
-app.include_router(users_router)  
 
 @app.get("/health")
 def root():
