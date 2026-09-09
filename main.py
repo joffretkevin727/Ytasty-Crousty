82e9817fbbe53f6fb8710af6199b0d2bf8592c0d
Joffret kevin <joffretkevin727@gmail.com>
2026-09-09 17:59:58 +0200

    remove line unuse

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from fastapi import FastAPI
-
-<<<<<<< HEAD
 from src.app.modules.auth.router import auth_router
 
 app = FastAPI(title="Toasty-Crousty")
-=======
-app = FastAPI(title="Ytasty-Crousty")
->>>>>>> feature/common
-
 app.include_router(auth_router)  
 
 @app.get("/health")
