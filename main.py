0bc3548d63134c07c591b09a3674163809fd7d82
Joffret kevin <joffretkevin727@gmail.com>
2026-09-09 17:29:33 +0200

    feature: health route operationnal

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
+from app.modules.health.router import router as health_router
 
 app = FastAPI(title="Toasty-Crousty")
+app.include_router(health_router)
 
-print("test")
 
-@app.get("/health")
-def root():
-    return {"status": "ok"}
+print("L'API est en cours d'exécution sur http://localhost:8000")
+print("Pour vérifier l'état de l'API, accédez à http://localhost:8000/health")
+print("Pour accéder à la documentation interactive de l'API, accédez à http://localhost:8000/docs")
+
