305148d807e3bdf5b13bb0e8e7cd940db0025e02
Nikita Petrenko <nikita.petrenko@ynov.com>
2026-09-17 15:09:57 +0200

    remove prints from main

--- main.py
+++ main.py
@@ -9,8 +9,3 @@ app = FastAPI(title="Toasty-Crousty")
 app.include_router(health_router)
 app.include_router(auth_router) 
 app.include_router(users_router)  
-
-print("L'API est en cours d'exécution sur http://localhost:8000")
-print("Pour vérifier l'état de l'API, accédez à http://localhost:8000/health")
-print("Pour accéder à la documentation interactive de l'API, accédez à http://localhost:8000/docs")
-
